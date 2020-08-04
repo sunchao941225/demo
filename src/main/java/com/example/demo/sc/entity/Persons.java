73690209a3ba2b16b363b65525708f0718637fee
sunchao941225 <55977255+sunchao941225@users.noreply.github.com>
2020-08-04 10:23:24 +0800

    test

--- src/main/java/com/example/demo/sc/entity/Persons.java
+++ src/main/java/com/example/demo/sc/entity/Persons.java
@@ -1,10 +1,13 @@
 package com.example.demo.sc.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
 public class Persons {
     @Id
     @GeneratedValue
@@ -16,35 +19,5 @@ public class Persons {
 
     private String city;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
+    
 }
